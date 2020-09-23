d357d10488b10a12aec89e608eea69b3fbac6c6a
Al-Eryani,Mohammed M.A.A <m.aleryani@student.fontys.nl>
2020-09-23 15:00:32 +0200

    add skeleton classes for  Employee ,DepoWorker,SaleWorker

---
+++ Proj Application/Proj Desktop App/Proj Desktop App/DepotWorker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj_Desktop_App
+{
+    class DepotWorker:Employee
+    {
+        private string job;
+        public DepotWorker(int BSN, string firstName, string LastName, string gender, int phoneNumber, DateTime birthDate,
+                          string adress, string certificates, string status, string department, string contactEmail, double fte, string job)
+              : base(BSN, firstName, LastName, gender, phoneNumber, birthDate, adress, certificates, status, department, contactEmail, fte)
+        {
+            this.job = job;
+        }
+    }
+}
---
+++ Proj Application/Proj Desktop App/Proj Desktop App/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj_Desktop_App
+{
+    class Employee
+    {
+        private int BSN;
+        private string firstName;
+        private string LastName;
+        private string gender;
+        private int phoneNumber;
+        private DateTime birthDate;
+        private string adress;
+        private string certificates;
+        private string status;
+        private string department;
+        private string contactEmail;
+        private double fte;
+
+        public Employee(int BSN, string firstName, string LastName, string gender, int phoneNumber, DateTime birthDate,
+            string adress, string certificates,string status, string department, string contactEmail,double fte)
+        {
+            this.BSN = BSN;
+            this.LastName = LastName;
+            this.firstName = firstName;
+            this.gender = gender;
+            this.phoneNumber = phoneNumber;
+            this.birthDate = birthDate;
+            this.adress = adress;
+            this.certificates = certificates;
+            this.status = status;
+            this.department = department;
+            this.contactEmail = contactEmail;
+            this.fte = fte;
+        }
+    }
+}
