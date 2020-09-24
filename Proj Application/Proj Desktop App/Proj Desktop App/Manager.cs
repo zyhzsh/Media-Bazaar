d79738c50416613d9f5ba7ae01c71e0586107bb0
JoostBee <j.burggraaff@student.fontys.nl>
2020-09-24 22:49:49 +0200

    Added manager& departments
    Added manager class and departments enum

---
+++ Proj Application/Proj Desktop App/Proj Desktop App/Manager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj_Desktop_App
+{
+    class Manager : Employee
+    {
+        string managerSpecificInfo;
+
+        public Manager(int BSN, string firstName, string LastName, string gender, int phoneNumber, DateTime birthDate,
+                  string adress, string certificates, string status, string department, string contactEmail, double fte, string managerSpecificInfo)
+      : base(BSN, firstName, LastName, gender, phoneNumber, birthDate, adress, certificates, status, department, contactEmail, fte)
+        {
+            this.managerSpecificInfo = managerSpecificInfo;
+        }
+    }
+}
