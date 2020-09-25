e2c94d8e43df6e6be5f39e3147bd6b13806b944a
Lima Ruas van Rijthoven,David D <d.limaruasvanrijthoven@student.fontys.nl>
2020-09-25 17:08:09 +0200

    merge conflicts

--- Proj Application/Proj Desktop App/Proj Desktop App/Form1.cs
+++ Proj Application/Proj Desktop App/Proj Desktop App/Form1.cs
@@ -24,34 +24,7 @@ namespace Proj_Desktop_App
 
         private void btLogin_Click(object sender, EventArgs e)
         {
-            if (login.CheckLogin(textBox1.Text, textBox2.Text))
-            {
-                PositionType positionType = login.CheckPossition;
-                switch(positionType){
-                    case PositionType.Administrator:
-                        //open admin form
-                        break;
-                    case PositionType.Depot_Manager:
-                        //open depot_man form
-                        //EmployeeStatistics employeeStatistics = new EmployeeStatistics();
-                        //employeeStatistics.Visible = true;
-                        // OR
-                        //make button visisble
-                        //btStatistics.Visible = true;
-                        break;
-                    case PositionType.Sales_Manager:
-                        //open Sale_manager form
-                        break;
-                    case PositionType.Depot_Worker:
-                        //open Depot_worker form
-                        break;
-                }
-                this.Visible = false;
-            }
-            else
-            {
-                MessageBox.Show("Incorrect username or password.");
-            }
+           
         }
 
         private void btStatistics_Click(object sender, EventArgs e)
