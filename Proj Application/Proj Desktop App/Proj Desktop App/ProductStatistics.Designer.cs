0cc43f04569ec4d05f47246f42b0c0278b44e005
Al-Eryani,Mohammed M.A.A <m.aleryani@student.fontys.nl>
2020-09-25 16:13:27 +0200

    Revert "Merge branch 'revert-132cbdd3' into 'master'"
    This reverts merge request !1

--- Proj Application/Proj Desktop App/Proj Desktop App/ProductStatistics.Designer.cs
+++ Proj Application/Proj Desktop App/Proj Desktop App/ProductStatistics.Designer.cs
@@ -28,132 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox9 = new System.Windows.Forms.TextBox();
-            this.label16 = new System.Windows.Forms.Label();
-            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.btnProductSearch = new System.Windows.Forms.Button();
+            this.cbProductSearchMethod = new System.Windows.Forms.ComboBox();
+            this.tbProductSearchAttribute = new System.Windows.Forms.TextBox();
+            this.tbSelectedProductStock = new System.Windows.Forms.TextBox();
             this.label17 = new System.Windows.Forms.Label();
-            this.textBox8 = new System.Windows.Forms.TextBox();
-            this.label15 = new System.Windows.Forms.Label();
             this.label14 = new System.Windows.Forms.Label();
-            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.tbSelectedProductSales = new System.Windows.Forms.TextBox();
             this.listBox4 = new System.Windows.Forms.ListBox();
+            this.cbSalesLastTime = new System.Windows.Forms.ComboBox();
+            this.btnRequestRestock = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox2
+            // btnProductSearch
             // 
-            this.textBox2.Location = new System.Drawing.Point(697, 215);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 42;
+            this.btnProductSearch.Location = new System.Drawing.Point(243, 61);
+            this.btnProductSearch.Name = "btnProductSearch";
+            this.btnProductSearch.Size = new System.Drawing.Size(96, 24);
+            this.btnProductSearch.TabIndex = 40;
+            this.btnProductSearch.Text = "Search";
+            this.btnProductSearch.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // cbProductSearchMethod
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(589, 218);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(102, 17);
-            this.label1.TabIndex = 41;
-            this.label1.Text = "Last promotion";
+            this.cbProductSearchMethod.FormattingEnabled = true;
+            this.cbProductSearchMethod.Items.AddRange(new object[] {
+            "Product code",
+            "Product name",
+            "Most Sold"});
+            this.cbProductSearchMethod.Location = new System.Drawing.Point(10, 59);
+            this.cbProductSearchMethod.Name = "cbProductSearchMethod";
+            this.cbProductSearchMethod.Size = new System.Drawing.Size(121, 24);
+            this.cbProductSearchMethod.TabIndex = 39;
+            this.cbProductSearchMethod.Text = "Product code";
             // 
-            // button1
+            // tbProductSearchAttribute
             // 
-            this.button1.Location = new System.Drawing.Point(243, 61);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(96, 24);
-            this.button1.TabIndex = 40;
-            this.button1.Text = "Search";
-            this.button1.UseVisualStyleBackColor = true;
+            this.tbProductSearchAttribute.Location = new System.Drawing.Point(137, 61);
+            this.tbProductSearchAttribute.Name = "tbProductSearchAttribute";
+            this.tbProductSearchAttribute.Size = new System.Drawing.Size(100, 22);
+            this.tbProductSearchAttribute.TabIndex = 38;
+            this.tbProductSearchAttribute.Text = "122324";
+            this.tbProductSearchAttribute.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // comboBox1
+            // tbSelectedProductStock
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "BSN",
-            "First name",
-            "Last name",
-            "Phone"});
-            this.comboBox1.Location = new System.Drawing.Point(10, 59);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 39;
-            this.comboBox1.Text = "Employee BSN";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(137, 61);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 38;
-            this.textBox1.Text = "531223";
-            // 
-            // textBox9
-            // 
-            this.textBox9.Location = new System.Drawing.Point(697, 187);
-            this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(100, 22);
-            this.textBox9.TabIndex = 37;
-            // 
-            // label16
-            // 
-            this.label16.AutoSize = true;
-            this.label16.Location = new System.Drawing.Point(643, 190);
-            this.label16.Name = "label16";
-            this.label16.Size = new System.Drawing.Size(48, 17);
-            this.label16.TabIndex = 36;
-            this.label16.Text = "Salary";
-            // 
-            // textBox10
-            // 
-            this.textBox10.Location = new System.Drawing.Point(697, 104);
-            this.textBox10.Name = "textBox10";
-            this.textBox10.Size = new System.Drawing.Size(100, 22);
-            this.textBox10.TabIndex = 35;
+            this.tbSelectedProductStock.Location = new System.Drawing.Point(604, 92);
+            this.tbSelectedProductStock.Name = "tbSelectedProductStock";
+            this.tbSelectedProductStock.Size = new System.Drawing.Size(55, 22);
+            this.tbSelectedProductStock.TabIndex = 35;
+            this.tbSelectedProductStock.Text = "50";
             // 
             // label17
             // 
             this.label17.AutoSize = true;
-            this.label17.Location = new System.Drawing.Point(633, 107);
+            this.label17.Location = new System.Drawing.Point(555, 92);
             this.label17.Name = "label17";
-            this.label17.Size = new System.Drawing.Size(58, 17);
+            this.label17.Size = new System.Drawing.Size(43, 17);
             this.label17.TabIndex = 34;
-            this.label17.Text = "Position";
-            // 
-            // textBox8
-            // 
-            this.textBox8.Location = new System.Drawing.Point(697, 159);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(100, 22);
-            this.textBox8.TabIndex = 33;
-            // 
-            // label15
-            // 
-            this.label15.AutoSize = true;
-            this.label15.Location = new System.Drawing.Point(592, 162);
-            this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(99, 17);
-            this.label15.TabIndex = 32;
-            this.label15.Text = "Shifts no show";
+            this.label17.Text = "Stock";
             // 
             // label14
             // 
             this.label14.AutoSize = true;
-            this.label14.Location = new System.Drawing.Point(616, 134);
+            this.label14.Location = new System.Drawing.Point(555, 169);
             this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(75, 17);
+            this.label14.Size = new System.Drawing.Size(108, 17);
             this.label14.TabIndex = 31;
-            this.label14.Text = "Shifts Late";
+            this.label14.Text = "Sales in the last";
             // 
-            // textBox7
+            // tbSelectedProductSales
             // 
-            this.textBox7.Location = new System.Drawing.Point(697, 131);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(100, 22);
-            this.textBox7.TabIndex = 30;
+            this.tbSelectedProductSales.Location = new System.Drawing.Point(821, 166);
+            this.tbSelectedProductSales.Name = "tbSelectedProductSales";
+            this.tbSelectedProductSales.Size = new System.Drawing.Size(168, 22);
+            this.tbSelectedProductSales.TabIndex = 30;
+            this.tbSelectedProductSales.Text = "129";
             // 
             // listBox4
             // 
@@ -169,24 +119,43 @@
             this.listBox4.Size = new System.Drawing.Size(539, 564);
             this.listBox4.TabIndex = 29;
             // 
+            // cbSalesLastTime
+            // 
+            this.cbSalesLastTime.FormattingEnabled = true;
+            this.cbSalesLastTime.Items.AddRange(new object[] {
+            "Week",
+            "30 Days",
+            "3 Months",
+            "Year",
+            "All time"});
+            this.cbSalesLastTime.Location = new System.Drawing.Point(679, 166);
+            this.cbSalesLastTime.Name = "cbSalesLastTime";
+            this.cbSalesLastTime.Size = new System.Drawing.Size(121, 24);
+            this.cbSalesLastTime.TabIndex = 43;
+            // 
+            // btnRequestRestock
+            // 
+            this.btnRequestRestock.Location = new System.Drawing.Point(558, 120);
+            this.btnRequestRestock.Name = "btnRequestRestock";
+            this.btnRequestRestock.Size = new System.Drawing.Size(101, 43);
+            this.btnRequestRestock.TabIndex = 44;
+            this.btnRequestRestock.Text = "Request Restock";
+            this.btnRequestRestock.UseVisualStyleBackColor = true;
+            // 
             // ProductStatistics
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1185, 748);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.textBox9);
-            this.Controls.Add(this.label16);
-            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.btnRequestRestock);
+            this.Controls.Add(this.cbSalesLastTime);
+            this.Controls.Add(this.btnProductSearch);
+            this.Controls.Add(this.cbProductSearchMethod);
+            this.Controls.Add(this.tbProductSearchAttribute);
+            this.Controls.Add(this.tbSelectedProductStock);
             this.Controls.Add(this.label17);
-            this.Controls.Add(this.textBox8);
-            this.Controls.Add(this.label15);
             this.Controls.Add(this.label14);
-            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.tbSelectedProductSales);
             this.Controls.Add(this.listBox4);
             this.Name = "ProductStatistics";
             this.Text = "ProductStatistics";
@@ -196,20 +165,15 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox9;
-        private System.Windows.Forms.Label label16;
-        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Button btnProductSearch;
+        private System.Windows.Forms.ComboBox cbProductSearchMethod;
+        private System.Windows.Forms.TextBox tbProductSearchAttribute;
+        private System.Windows.Forms.TextBox tbSelectedProductStock;
         private System.Windows.Forms.Label label17;
-        private System.Windows.Forms.TextBox textBox8;
-        private System.Windows.Forms.Label label15;
         private System.Windows.Forms.Label label14;
-        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox tbSelectedProductSales;
         private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ComboBox cbSalesLastTime;
+        private System.Windows.Forms.Button btnRequestRestock;
     }
 }
