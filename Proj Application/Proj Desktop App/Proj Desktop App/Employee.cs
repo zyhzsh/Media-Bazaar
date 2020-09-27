6956b7405f9ea49eaa5d1c7e2465cd5a3ad0ec1a
Martin Dzhurov <m.dzhurov@student.fontys.nl>
2020-09-27 13:16:28 +0200

    Employee CRUD

--- Proj Application/Proj Desktop App/Proj Desktop App/Employee.cs
+++ Proj Application/Proj Desktop App/Proj Desktop App/Employee.cs
@@ -6,38 +6,69 @@ using System.Threading.Tasks;
 
 namespace Proj_Desktop_App
 {
-    class Employee
-    {
+   public class Employee
+   {
         private int BSN;
         private string firstName;
-        private string LastName;
-        private string gender;
-        private int phoneNumber;
+        private string lastName;
+        private char gender;
+        private string phoneNumber;
         private DateTime birthDate;
-        private string adress;
+        private string address;
         private string certificates;
         private string status;
-        private string department;
+        private Departments department;
         private string contactEmail;
         private PositionType positionType;
+        private string jobTitle;
         private double fte;
 
-        public Employee(int BSN, string firstName, string LastName, string gender, int phoneNumber, DateTime birthDate,
-            string adress, string certificates,string status, string department, string contactEmail,double fte, PositionType positionType)
+        public Employee(int BSN, string firstName, string lastName, char gender, string phoneNumber, DateTime birthDate,
+            string address, string certificates, string status, Departments department, string contactEmail,
+            double fte, PositionType positionType, string jobTitle)
         {
             this.BSN = BSN;
-            this.LastName = LastName;
+            this.lastName = lastName;
             this.firstName = firstName;
             this.gender = gender;
             this.phoneNumber = phoneNumber;
             this.birthDate = birthDate;
-            this.adress = adress;
+            this.address = address;
             this.certificates = certificates;
             this.status = status;
             this.department = department;
             this.contactEmail = contactEmail;
             this.fte = fte;
             this.positionType = positionType;
+            this.jobTitle = jobTitle;
         }
-    }
+
+        public int GetBSN()
+        {
+            return BSN;
+        }
+
+        public string GetInfo()
+        {
+            return $"{BSN} - {lastName}, {department}, {contactEmail}";
+        }
+
+        public void UpdateInfo()
+        {
+            // to be added
+        }
+
+        public void UpdateStatus(string newStatus)
+        {
+            status = newStatus;
+        }
+
+        public override string ToString()
+        {
+            return $"{BSN} - {firstName} {lastName}, gender: {gender}, birthdate: {birthDate.ToShortDateString()}\n" +
+                $"phone: {phoneNumber}, address: {address}, email: {contactEmail}\n" +
+                $"department: {department}, position: {positionType}, jobTitle: {jobTitle}, fte: {fte}, status: {status}, certificates: {certificates}";
+        }
+
+   }
 }
