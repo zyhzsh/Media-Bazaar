da931f434deba449c36cfbeff13493307c5e7986
wjzyhzsh <wjzyhzsh@outlook.com>
2020-09-29 11:01:39 +0200

    UpDating Shift GUI and implement some Features

--- Proj Application/Proj Desktop App/Proj Desktop App/Form1.Designer.cs
+++ Proj Application/Proj Desktop App/Proj Desktop App/Form1.Designer.cs
@@ -36,26 +36,28 @@
             this.button2 = new System.Windows.Forms.Button();
             this.btEmployeeStatics = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-
             this.SuspendLayout();
-            //LoginUsername 
+            // 
+            // tbLogInUsername
+            // 
             this.tbLogInUsername.Location = new System.Drawing.Point(85, 21);
             this.tbLogInUsername.Name = "tbLogInUsername";
-            this.tbLogInUsername.Size = new System.Drawing.Size(160, 22);
+            this.tbLogInUsername.Size = new System.Drawing.Size(160, 20);
             this.tbLogInUsername.TabIndex = 0;
-            //Password
+            // 
+            // tbLogInPassword
+            // 
             this.tbLogInPassword.Location = new System.Drawing.Point(85, 47);
             this.tbLogInPassword.Name = "tbLogInPassword";
             this.tbLogInPassword.PasswordChar = '*';
-            this.tbLogInPassword.Size = new System.Drawing.Size(160, 22);
+            this.tbLogInPassword.Size = new System.Drawing.Size(160, 20);
             this.tbLogInPassword.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 20);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Location = new System.Drawing.Point(9, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(55, 13);
             this.label1.TabIndex = 2;
@@ -64,9 +66,8 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(21, 41);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Location = new System.Drawing.Point(9, 50);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 3;
@@ -82,8 +83,8 @@
             this.btLogin.Text = "Log In";
             this.btLogin.UseVisualStyleBackColor = true;
             this.btLogin.Click += new System.EventHandler(this.btLogin_Click);
-                // 
-            // btnOpenScheduling
+            // 
+            // button2
             // 
             this.button2.Location = new System.Drawing.Point(20, 122);
             this.button2.Margin = new System.Windows.Forms.Padding(2);
@@ -92,6 +93,7 @@
             this.button2.TabIndex = 5;
             this.button2.Text = "Schedule";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // btEmployeeStatics
             // 
@@ -104,10 +106,7 @@
             this.btEmployeeStatics.UseVisualStyleBackColor = true;
             this.btEmployeeStatics.Click += new System.EventHandler(this.btStatistics_Click);
             // 
-            // btnOpenEmpStatistics
-            // 
-            // 
-            // btnOpenDptmtManagement
+            // button4
             // 
             this.button4.Location = new System.Drawing.Point(728, 122);
             this.button4.Margin = new System.Windows.Forms.Padding(2);
@@ -121,19 +120,16 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(934, 572);
+            this.ClientSize = new System.Drawing.Size(1246, 704);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.btEmployeeStatics);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.btLogin);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2);
-            this.ClientSize = new System.Drawing.Size(1246, 704);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.tbLogInPassword);
             this.Controls.Add(this.tbLogInUsername);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Manager Application";
             this.ResumeLayout(false);
---
+++ Proj Application/Proj Desktop App/Proj Desktop App/Schedule.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj_Desktop_App
+{
+    class Schedule
+    {
+        List<AssignedShift> assignedShifts;
+        public Schedule()
+        {
+            //Read for somewhere or database
+            assignedShifts = new List<AssignedShift>();
+       
+        }
+        public  bool AssignEmployeeToShift(ShiftType shiftType,string selecteddate,List<Employee>employees)
+        {  
+            foreach (AssignedShift e in assignedShifts)
+            {
+                assignedShifts.Add(new AssignedShift(selecteddate,shiftType,employees));
+            }   
+            return true;        
+        }
+
+        public bool AssignEmployeeToShift(ShiftType shiftType, string selecteddate, Employee employee)
+        {
+
+            ////情况一：所选的日期没有返回值
+            //if (GetAssignedEmployees(selecteddate).Count() == 0)
+            //{
+            //    assignedShifts.Add(new AssignedShift(selecteddate, shiftType, employee));
+            //}
+            ////情况二：所选日期有返回值
+            //else {
+            //    List<Employee>emp = GetAssignedEmployees(selecteddate);
+
+            //    //1.若是返回的值内部有这个员工
+            //    if (emp.Contains(employee))
+            //    { 
+            //    //检测员工的shiftype是否一致
+                
+
+
+            //    //1.如果一致             
+                
+            //    }
+            //}
+
+
+            ////1.输入时间、和SHIFT类型、和员工
+            ////2.通过输入的时间来获得当天的列表员工列表
+            ////3.在此基础上更新
+            //foreach (AssignedShift e in assignedShifts)
+            //{
+            //    if (e.SelectedDate == selecteddate)
+            //    { 
+                   
+            //    }
+            //}
+            
+            
+            return true;
+
+        }
+
+
+
+
+
+
+
+        public bool RemoveFromShift(ShiftType shiftType, DateTime selecteddate, List<string> employee_bsn)
+        { 
+            return false;
+        }
+
+        public List<Employee> GetAssignedEmployees(string seleteddate)
+        {
+            List<Employee> tempo_empolyees = new List<Employee>();  
+            foreach (AssignedShift e in assignedShifts)
+            {
+                if (e.SelectedDate == seleteddate)
+                {
+                    tempo_empolyees = e.GetShiftsList();
+                }          
+            }
+            return tempo_empolyees;
+        }
+
+
+
+
+        public List<Shift> GetAvailableEmployees(DateTime seleteddate)
+        {
+            return null;
+        }
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
