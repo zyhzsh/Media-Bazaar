1d941e1f99e9bd2df0d1f9c37fc437300f9c6951
Martin Dzhurov <m.dzhurov@student.fontys.nl>
2020-10-05 15:13:10 +0200

    Small fix Employee Mock Data

--- Proj Application/Proj Desktop App/Proj Desktop App/Store.cs
+++ Proj Application/Proj Desktop App/Proj Desktop App/Store.cs
@@ -77,11 +77,11 @@ namespace Proj_Desktop_App
             AddEmployee(5467, "George", "Wood", 'M', "0031677777", DateTime.ParseExact("18/09/1997", format, cultureInf), "Eindhoven", "Administration ", "Employed", Departments.office, "g.wood@gmail.com", 1, PositionType.Administrator, "Administrator");
             AddEmployee(3456, "Nicole", "Green", 'F', "0031655378", DateTime.ParseExact("27/11/1998", format, cultureInf), "Geldrop", "IELTS", "Employed", Departments.warehouse, "n.green@gmail.com", 1, PositionType.Depot_Worker, "Depot_Worker");
             AddEmployee(7890, "John", "Doe", 'M', "0031674628", DateTime.ParseExact("05/12/1991", format, cultureInf), "Helmond", "Mangment ", "Employed", Departments.floorTwo, "john.d@gmail.com", 0.4, PositionType.Sales_Manager, "Sales Manager");
-            employees.Add(new Employee(3992616, "Shenghang", "Zhu", 'M', "134556", DateTime.Parse("01/01/2020"), "Tilburg", "ICT", "employed", Departments.warehouse, "xxxx@gmail.com", 1, PositionType.Depot_Worker, ""));
-            employees.Add(new Employee(3376540, "David", "van Rijthoven ", 'M', "134556", DateTime.Parse("01/01/2020"), "Eindhoven", "ICT", "employed", Departments.floorOne, "xxxx@gmail.com", 1, PositionType.Sales_Worker, ""));
-            employees.Add(new Employee(4090918, "Dzhurov", "M", 'M', "134556", DateTime.Parse("01/01/2020"), "Amersfoort", "ICT", "employed", Departments.floorOne, "xxxx@gmail.com", 1, PositionType.Depot_Worker, ""));
-            employees.Add(new Employee(443892, "Mohammed", "Al-Eryani", 'M', "134556", DateTime.Parse("01/01/2020"), "Den Haag", "ICT", "employed", Departments.floorFour, "xxxx@gmail.com", 1, PositionType.Sales_Worker, ""));
-            employees.Add(new Employee(3088685, "Joost", "Burggraaff", 'M', "134556", DateTime.Parse("01/01/2020"), "Eindhoven", "ICT", "employed", Departments.floorThree, "xxxx@gmail.com", 1, PositionType.Depot_Worker, ""));
+            AddEmployee(3992616, "Shenghang", "Zhu", 'M', "134556", DateTime.ParseExact("01/01/2020", format, cultureInf), "Tilburg", "ICT", "employed", Departments.warehouse, "xxxx@gmail.com", 1, PositionType.Depot_Worker, "");
+            AddEmployee(3376540, "David", "van Rijthoven ", 'M', "134556", DateTime.ParseExact("01/01/2020", format, cultureInf), "Eindhoven", "ICT", "employed", Departments.floorOne, "xxxx@gmail.com", 1, PositionType.Sales_Worker, "");
+            AddEmployee(4090918, "Dzhurov", "M", 'M', "134556", DateTime.ParseExact("01/01/2020", format, cultureInf), "Amersfoort", "ICT", "employed", Departments.floorOne, "xxxx@gmail.com", 1, PositionType.Depot_Worker, "");
+            AddEmployee(443892, "Mohammed", "Al-Eryani", 'M', "134556", DateTime.ParseExact("01/01/2020", format, cultureInf), "Den Haag", "ICT", "employed", Departments.floorFour, "xxxx@gmail.com", 1, PositionType.Sales_Worker, "");
+            AddEmployee(3088685, "Joost", "Burggraaff", 'M', "134556", DateTime.ParseExact("01/01/2020", format, cultureInf), "Eindhoven", "ICT", "employed", Departments.floorThree, "xxxx@gmail.com", 1, PositionType.Depot_Worker, "");
         }
 
         private void ProductMockData()
