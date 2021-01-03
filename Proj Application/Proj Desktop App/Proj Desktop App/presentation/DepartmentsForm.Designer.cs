51f997cb5009a429368f781ecd2d88caf1448bf7
Martin Dzhurov <m.dzhurov@student.fontys.nl>
2021-01-03 19:51:49 +0200

    Start of Departments Form

---
+++ Proj Application/Proj Desktop App/Proj Desktop App/presentation/DepartmentsForm.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace Proj_Desktop_App.presentation
+{
+    partial class DepartmentsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbDepartment = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lvEmployees = new System.Windows.Forms.ListView();
+            this.lvProducts = new System.Windows.Forms.ListView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lbEmployees = new System.Windows.Forms.Label();
+            this.lbProducts = new System.Windows.Forms.Label();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbDepartment
+            // 
+            this.cbDepartment.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbDepartment.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbDepartment.FormattingEnabled = true;
+            this.cbDepartment.Location = new System.Drawing.Point(128, 25);
+            this.cbDepartment.Name = "cbDepartment";
+            this.cbDepartment.Size = new System.Drawing.Size(175, 28);
+            this.cbDepartment.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Department:";
+            // 
+            // lvEmployees
+            // 
+            this.lvEmployees.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvEmployees.HideSelection = false;
+            this.lvEmployees.Location = new System.Drawing.Point(21, 108);
+            this.lvEmployees.Name = "lvEmployees";
+            this.lvEmployees.Size = new System.Drawing.Size(282, 314);
+            this.lvEmployees.TabIndex = 2;
+            this.lvEmployees.UseCompatibleStateImageBehavior = false;
+            // 
+            // lvProducts
+            // 
+            this.lvProducts.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvProducts.HideSelection = false;
+            this.lvProducts.Location = new System.Drawing.Point(340, 108);
+            this.lvProducts.Name = "lvProducts";
+            this.lvProducts.Size = new System.Drawing.Size(282, 314);
+            this.lvProducts.TabIndex = 3;
+            this.lvProducts.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(484, 25);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(138, 28);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add department";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // lbEmployees
+            // 
+            this.lbEmployees.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbEmployees.AutoSize = true;
+            this.lbEmployees.Location = new System.Drawing.Point(18, 87);
+            this.lbEmployees.Name = "lbEmployees";
+            this.lbEmployees.Size = new System.Drawing.Size(86, 18);
+            this.lbEmployees.TabIndex = 5;
+            this.lbEmployees.Text = "Employees:";
+            // 
+            // lbProducts
+            // 
+            this.lbProducts.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbProducts.AutoSize = true;
+            this.lbProducts.Location = new System.Drawing.Point(337, 87);
+            this.lbProducts.Name = "lbProducts";
+            this.lbProducts.Size = new System.Drawing.Size(72, 18);
+            this.lbProducts.TabIndex = 6;
+            this.lbProducts.Text = "Products:";
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.Location = new System.Drawing.Point(340, 25);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(138, 28);
+            this.btnEdit.TabIndex = 7;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // DepartmentsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 444);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.lbProducts);
+            this.Controls.Add(this.lbEmployees);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lvProducts);
+            this.Controls.Add(this.lvEmployees);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbDepartment);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "DepartmentsForm";
+            this.Text = "DepartmentsForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbDepartment;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView lvEmployees;
+        private System.Windows.Forms.ListView lvProducts;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lbEmployees;
+        private System.Windows.Forms.Label lbProducts;
+        private System.Windows.Forms.Button btnEdit;
+    }
+}
